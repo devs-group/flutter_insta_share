06c7fbf2bd7017aac0d3b151478473a1c62da3b7
Ralph Segi <ralph@segidev.com>
2020-05-03 14:17:30 +0200

    Set return value of int

--- lib/instashare.dart
+++ lib/instashare.dart
@@ -3,10 +3,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Instashare {
-  static const MethodChannel _channel =
-      const MethodChannel('instashare');
+  static const MethodChannel _channel = const MethodChannel('instashare');
 
-  static Future<void> shareToFeedInstagram(String type, String path) async {
+  static Future<int> shareToFeedInstagram(String type, String path) async {
     return _channel.invokeMethod('shareToFeedInstagram', <String, dynamic>{
       'type': type,
       'path': path,
